8c79f6e6dd2887e49e3acad22aa023243d3dcc98
ricardoatriana <ricardoatriana@hotmail.com>
2020-07-26 13:53:35 +0800

    Updates

---
+++ precache-manifest.8c4486c8ce3e2331a52861f3985a8cff.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7475041455bc1449add43439c63c3cb9",
+    "url": "/parchac/index.html"
+  },
+  {
+    "revision": "cad01bdb6d5921614951",
+    "url": "/parchac/static/css/2.fcc35274.chunk.css"
+  },
+  {
+    "revision": "708d68dd3f7ab1e2adfe",
+    "url": "/parchac/static/css/main.1a8943b8.chunk.css"
+  },
+  {
+    "revision": "cad01bdb6d5921614951",
+    "url": "/parchac/static/js/2.abfc80e8.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/parchac/static/js/2.abfc80e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708d68dd3f7ab1e2adfe",
+    "url": "/parchac/static/js/main.54cbeb4e.chunk.js"
+  },
+  {
+    "revision": "b803fadc74670fdcec0c",
+    "url": "/parchac/static/js/runtime-main.34dd0c6c.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/parchac/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/parchac/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/parchac/static/media/date-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/parchac/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/parchac/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/parchac/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "2545ae7ca7f967e854e5e3c958684c60",
+    "url": "/parchac/static/media/logo-white.2545ae7c.png"
+  },
+  {
+    "revision": "2545ae7ca7f967e854e5e3c958684c60",
+    "url": "/parchac/static/media/logo.2545ae7c.png"
+  }
+]);
